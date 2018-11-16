a66ba171ff7a5ed42fcfa1b18a6ba10f8b76152d
Mattias Liljenzin <mattias.liljenzin@gmail.com>
2018-11-16 12:36:51 +0100

    feat: remove logging and Serilog dependency

--- MhLabsLambdaEntryPointBase.cs
+++ MhLabsLambdaEntryPointBase.cs
@@ -1,54 +1,27 @@
-using System;
 using System.Collections.Generic;
-using System.IO;
-using System.Reflection;
 using System.Threading;
 using System.Threading.Tasks;
 using Amazon.Lambda;
 using Amazon.Lambda.APIGatewayEvents;
 using Amazon.Lambda.AspNetCoreServer;
 using Amazon.Lambda.Model;
-using MhLabs.SerilogExtensions;
-using Microsoft.AspNetCore.Hosting;
 using Newtonsoft.Json;
-using Serilog;
 
 namespace MhLabs.APIGatewayLambdaProxy
 {
     public abstract class LambdaEntryPointBase : APIGatewayProxyFunction
     {
-        protected const string CorrelationIdHeader = "mh-correlation-id";
         private const string Concurrency = "__CONCURRENCY__";
         private const string KeepAliveInvocation = "__KEEP_ALIVE_INVOCATION__";
         private readonly IAmazonLambda _lambda;
 
         private static bool Warm { get; set; }
-        private static string _correlationId;
 
-        public LambdaEntryPointBase() : base()
+        protected LambdaEntryPointBase() : base()
         {
             _lambda = new AmazonLambdaClient();
         }
-
-        protected abstract IWebHostBuilder Initialize(IWebHostBuilder builder);
-
-        protected virtual void UseCorrelationId(string correlationId) { }
-        protected override void Init(IWebHostBuilder builder)
-        {
-            Initialize(builder)
-                .UseSerilog((hostingContext, loggerConfiguration) =>
-                {
-                    loggerConfiguration
-                    .Enrich.With<MemberIdEnrichment>()
-                    .Enrich.With<ExceptionEnricher>()
-                    .Enrich.With<CorrelationIdEnrichment>()
-                    .Enrich.With<XRayEnrichment>()
-                    .Enrich.With<RequestBodyEnrichment>()
-                    .WriteTo.Console(outputTemplate: Constants.OutputTemplateFormat);
-                })
-                .UseApiGateway();
-        }
-
+        
         public override async Task<APIGatewayProxyResponse> FunctionHandlerAsync(APIGatewayProxyRequest request, Amazon.Lambda.Core.ILambdaContext lambdaContext)
         {
             if (string.IsNullOrEmpty(request.HttpMethod)) // For backward compability
@@ -67,14 +40,14 @@ namespace MhLabs.APIGatewayLambdaProxy
 
                         await Task.WhenAll(tasks);
                     }
-                    if (request.Headers.ContainsKey(KeepAliveInvocation)) 
+                    if (request.Headers.ContainsKey(KeepAliveInvocation))
                     {
                         Thread.Sleep(75); // To mitigate lambda reuse
                     }
                 }
 
                 lambdaContext.Logger.Log(JsonConvert.SerializeObject(request));
-                
+
                 if (Warm)
                 {
                     lambdaContext.Logger.Log("ping");
@@ -86,15 +59,6 @@ namespace MhLabs.APIGatewayLambdaProxy
                 request.Headers = new Dictionary<string, string> { { "Host", "localhost" } };
                 request.RequestContext = new APIGatewayProxyRequest.ProxyRequestContext();
                 lambdaContext.Logger.LogLine("Keep-alive invokation");
-            }
-            else
-            {
-                SetCorrelationId(request);
-                UseCorrelationId(_correlationId);
-                Console.WriteLine(JsonConvert.SerializeObject(request));
-                LogValueResolver.Register<MemberIdEnrichment>(() => request.RequestContext.Authorizer.Claims["custom:memberid"]);
-                LogValueResolver.Register<CorrelationIdEnrichment>(() => _correlationId);
-                LogValueResolver.Register<RequestBodyEnrichment>(() => request.Body);
             }
 
             Warm = true;
@@ -109,18 +73,6 @@ namespace MhLabs.APIGatewayLambdaProxy
                 FunctionName = System.Environment.GetEnvironmentVariable("AWS_LAMBDA_FUNCTION_NAME"),
                 Payload = "{\"Headers\":{\"" + KeepAliveInvocation + "\": \"1\"}}"
             };
-        }
-
-        private static void SetCorrelationId(APIGatewayProxyRequest request)
-        {
-            _correlationId = HasCorrelationIdHeader(request)
-                ? request.Headers[CorrelationIdHeader]
-                : Guid.NewGuid().ToString();
-        }
-
-        private static bool HasCorrelationIdHeader(APIGatewayProxyRequest request)
-        {
-            return request.Headers.ContainsKey(CorrelationIdHeader) && !string.IsNullOrEmpty(request.Headers[CorrelationIdHeader]);
         }
     }
 }
